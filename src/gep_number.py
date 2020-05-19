5b6c8b8b23875e394ae4d9bbb2910c1a9b91bc1f
Adam Amos-Binks <aabinks@gmail.com>
2020-05-19 09:03:44 -0400

    Updated gep_number.py for testing

--- src/gep_number.py
+++ src/gep_number.py
@@ -14,17 +14,18 @@ import grd_planning, grd_defs
 #import pyperplan._parse, pyperplan._ground
 from pddl.parser import Parser
 import grounding
+import time
 
 def get_ground_task(domain_filepath, problem_filepath):
     parser = Parser(domain_filepath, problem_filepath)
     domain = parser.parse_domain()
     problem = parser.parse_problem(domain)
     task = grounding.ground(problem)
+    
     return task
 
-def gep_number(domain_filepath, problem_filepath):
-
-    task = get_ground_task(domain_filepath, problem_filepath)
+def gep_number(ground_task):
+    
     effects = []
     gep_number = 0
     already_counted = False
@@ -41,20 +42,36 @@ def gep_number(domain_filepath, problem_filepath):
                 gep_number += 1
                 already_counted = True
 
-  return gep_number
+    return gep_number
 
+def gep_number_matrix(domain_path):
+    dirs = [x[1] for x in os.walk(os.path.dirname(domain_path))][0]
+    problem_names = [dir if dir.beginswith("p") for dir in dirs]
+    #problem_dirs.remove("results")
+    #problem_dirs.remove("domains")
+    gep_results_df = pd.Dataframe()
+
+    #check output dir
+    for domain_file in os.listdir(domain_path):
+        if (domain_file.endswith(".pddl")):
+            for problem_name in problem_names:
+                problem_filepath = os.path.join(*[os.path.dirname(domain_path),problem_name,"hyp_0.pddl" ])
+                task = get_ground_task(domain_filepath, problem_filepath)
+                gep_number = gep_number(task)      
+                gep_results_df = pd.concat([gep_results_df, pd.Dataframe(data={"domain":domain_file.split(".")[0], "problem":problem_name, "gep_number":gep_number})])
+
+    gep_matrix_df = gep_results_df.pivot(columns="problem", values="gep_number")
+    
+    return gep_matrix_df
+    
 if __name__=="__main__": 
     if len(sys.argv) < 2:
-        print("Usage: gep_number domain_folder")
+        print("Usage: gep_number domain_path")
         sys.exit()
     else:
-        domain_folder = sys.argv[1]
+        domain_path = sys.argv[1]
+        output_filename = "_".join([time.strftime("%Y%m%d_%H%M%S"), "all_probs_gep_numbers.csv"])
+        gep_matrix_df = gep_number_matrix(domain_path)
+        print(gep_matrix_df)
         
-        gep_results_df = pd.Dataframe()
-        
-        #check output dir
-        #TODO for each problem
-        #TODO for each domain in each problem
-        gep_number = gep_number(domain_filepath, problem_filepath)
-        gep_results_df = pd.concat([gep_results_df, pd.Dataframe(data=[domain_filepath, problem_filepath, gep_number])])
-        
+        gep_matrix_df.to_csv( os.path.join(domain_path, output_filename) )
